2edb48b5e9058afa796aad7c991dff99992d0e4b
mariev619 <marie.vendran@gmail.com>
2022-03-02 15:56:48 +0100

    index is up to date

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HeaderComponent } from './header/header.component';
 import { BodyComponent } from './body/body.component';
+import { DetailsComponent } from './details/details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     HeaderComponent,
-    BodyComponent
+    BodyComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule,
